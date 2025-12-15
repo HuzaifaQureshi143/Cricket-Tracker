f9e20bd11200c22f043e1b0ab08f1c5218c293ac
Huzaifa <231975@students.au.edu.pk>
2025-12-15 22:55:12 +0500

    test added

---
+++ src/test/java/com/crickettracker/CricketTrackerTest.java
@@ -0,0 +1,14 @@
+package com.crickettracker;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CricketTrackerTest {
+
+    @Test
+    public void sampleTest() {
+        int expected = 5;
+        int actual = 2 + 3;
+        assertEquals(expected, actual, "Simple addition test");
+    }
+}
